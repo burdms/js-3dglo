38447218633e02a7111f094c49c57b52bc32567b
burdms <burdms@gmail.com>
2021-04-21 22:28:10 +0200

    create: lesson18_optional-1 v.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line strict
   ('use strict');
+
+  function debounce(fn, delay) {
+    return function (event) {
+      console.log(this);
+      const previousCall = this.lastCall;
+      this.lastCall = Date.now();
+
+      if (previousCall && this.lastCall - previousCall <= delay) {
+        clearTimeout(this.lastCallTimer);
+      }
+
+      this.lastCallTimer = setTimeout(() => fn(event), delay);
+    };
+  }
+
+  function type(event) {
+    document.querySelector('p').textContent = event.target.value;
+  }
+
+  document
+    .querySelector('input')
+    .addEventListener('input', debounce(type, 300));
 });
