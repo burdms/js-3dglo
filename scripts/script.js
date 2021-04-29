10654796bbfac6b8a4da952c89284640753736de
burdms <burdms@gmail.com>
2021-04-29 22:16:51 +0200

    lesson24

--- scripts/script.js
+++ scripts/script.js
@@ -380,6 +380,49 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  // Calculator
+  function calculator(price) {
+    const calcBlock = document.querySelector('.calc-block');
+
+    calcBlock.addEventListener('input', event => {
+      const target = event.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum(price);
+      }
+    });
+  }
+
+  function countSum(price) {
+    let total = 0,
+      countValue = 1,
+      dayValue = 10;
+
+    const calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      calcTotal = document.getElementById('total'),
+      typeValue = calcType.options[calcType.selectedIndex].value,
+      squareValue = +calcSquare.value;
+
+    if (calcCount.value > 1) {
+      countValue += (calcCount.value - 1) / 10;
+    }
+
+    if (calcDay.value && calcDay.value < 5) {
+      dayValue *= 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      dayValue *= 1.5;
+    }
+
+    if (typeValue && squareValue) {
+      total = price * typeValue * squareValue * countValue * dayValue;
+    }
+
+    calcTotal.textContent = total;
+  }
+
   countTimer('22 april 2021');
   toggleMenu();
   togglePopup();
@@ -392,4 +435,5 @@ window.addEventListener('DOMContentLoaded', () => {
   checkEmailInputs();
   checkPhoneInputs();
   checkAllInputs();
+  calculator(100);
 });
